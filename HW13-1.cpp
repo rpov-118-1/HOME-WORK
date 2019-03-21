007b8ab2b0a66b69c0294335666711ddca81f41c
Blacit <39876617+Blacit@users.noreply.github.com>
2019-03-21 20:54:26 +0300

    Create HW13-1.cpp

---
+++ HW13-1.cpp
@@ -0,0 +1,45 @@
+#include "iostream"
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const size_t a = 10;
+	const size_t b = 10;
+	int arr[a] = {};
+	int arr2[b] = {};
+	int *pa = arr;
+	int *pb = arr2;
+
+	cout << "Random number in 'a' Massiv: ";
+	for (int i = 0; i < a; i++) 
+	{
+			arr[i] = rand() % 11;
+			cout << arr[i] << " ";
+	}
+
+	cout << endl << endl;
+	cout << "Massive A :" << endl;
+
+	for (int *pa = arr; pa <= &arr[a] - 1; pa++)
+	{
+		cout << "address= " << pa << "\tvalue= " << *pa << endl;
+	}
+
+	cout << endl << endl;
+
+	cout << "Massive B: " << endl;
+
+	for (int j = 0; j < 10; j++) 
+	{
+		(*(pb + j) = *(pa + j));
+	}
+
+	for (int j = 0; j < 10; j++)
+	{
+		cout << "address = " << (pb+j) << "\tvalue = " << *(pb+j) << endl;
+	} 
+	cout << endl << endl;
+	return 0;
+}
